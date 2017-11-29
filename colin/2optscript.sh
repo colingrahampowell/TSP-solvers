3b879120d0447adb787b4db4ddbf67308aff61bf
Colin Powell <colingrahampowell@gmail.com>
2017-11-28 21:30:00 -0800

    modifying/improving test script

--- colin/2optscript.sh
+++ colin/2optscript.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-now=`date '+%Y_%m_%d_%H'`
+# run three tests, store three different sets of results
+for k in `seq 1 3`
+do
 
-mkdir ex-sols-$now
+	# make a folder with today's date
+	now=`date '+%Y_%m_%d_%H_%M'`
+	mkdir ex-sols-$now
 
-tsp-2opt tsp_example_1.txt
-tsp-2opt tsp_example_2.txt
-2opt-pp tsp_example_3.txt
-python tsp-verifier.py tsp_example_1.txt tsp_example_1.txt.tour
-python tsp-verifier.py tsp_example_2.txt tsp_example_2.txt.tour
-python tsp-verifier.py tsp_example_3.txt tsp_example_3.txt.tour
+	# run the example cases (all three)
+	for i in `seq 1 3`;
+	do
+		echo "example_$i:" | tee -a log-$now
+		tsp-2opt tsp_example_$i.txt | tee -a log-$now
+		python tsp-verifier.py tsp_example_$i.txt tsp_example_$i.txt.tour | tee -a log-$now
+	done
 
-mv *.txt.tour ./ex-sols-$now
+	# now, run the timed competition cases (all seven)
+	for i in `seq 1 7`;
+	do
+		echo "test_$i:" | tee -a log-$now
+		tsp-2opt test-input-$i.txt -t | tee -a log-$now
+		python tsp-verifier.py test-input-$i.txt test-input-$i.txt.tour | tee -a log-$now
+	done
 
+	# move everything into our solutions folder
+	mv *.txt.tour ./ex-sols-$now
+	mv log-$now ./ex-sols-$now
+
+done
